d97403e71e847e7fb9b6655075b7b4eba6a14730
Cédric MAGNE <cmagne@ippon.fr>
2023-04-02 19:33:53 +0200

    Apply postgresql module

---
+++ src/main/java/opm/garden/technical/infrastructure/secondary/postgresql/DatabaseConfiguration.java
@@ -0,0 +1,10 @@
+package opm.garden.technical.infrastructure.secondary.postgresql;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@EnableTransactionManagement
+@EnableJpaRepositories(basePackages =  { "opm.garden" }, enableDefaultTransactions = false)
+public class DatabaseConfiguration {}
